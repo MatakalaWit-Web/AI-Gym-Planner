952433fc6a2d13df5c7247c2cf971242055be3f0
MatakalaWit-Web <matakalanew@gmail.com>
2026-03-15 23:28:03 +0200

    Add AuthProvider and user context for authentication management

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,29 @@ import Account from "./pages/account.tsx"
 import Navbar from "./componets/layout/navbar.tsx"
 import { NeonAuthUIProvider } from '@neondatabase/neon-js/auth/react';
 import { authClient } from "./lib/auth.ts"
+import AuthProvider from "./context/AuthContext.tsx"
 
 
 const App = () => {
   return (
     <div>
       <NeonAuthUIProvider authClient={authClient}>
-      <BrowserRouter>
-        <div className="min-h-screen flex flex-col">
-          <Navbar />
-          <main className="flex-1">
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/auth/:pathname" element={<Auth />} />
-              <Route path="/onboarding" element={<Onboarding />} />
-              <Route path="/account/:pathname" element={<Account />} />
-            </Routes>
-          </main>
-        </div>
-      </BrowserRouter>
+        <AuthProvider>
+          <BrowserRouter>
+            <div className="min-h-screen flex flex-col">
+              <Navbar />
+              <main className="flex-1">
+                <Routes>
+                  <Route index element={<Home />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/auth/:pathname" element={<Auth />} />
+                  <Route path="/onboarding" element={<Onboarding />} />
+                  <Route path="/account/:pathname" element={<Account />} />
+                </Routes>
+              </main>
+            </div>
+          </BrowserRouter>
+        </AuthProvider>
       </NeonAuthUIProvider>
     </div>
   )
